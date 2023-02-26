3c7efc34b3261c9e9acf295b298d904114a1657c
Brendan Cordova <cordovalegacy19@gmail.com>
2023-02-26 09:29:36 -0800

    Practice

--- deck_of_cards.js
+++ deck_of_cards.js
@@ -1,25 +1,21 @@
 class Deck {
     constructor() {
-        const suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-        const face = ['Ace', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        const face = ['Ace', '2', '3', '4', '5' ,'6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
         const deckOfCards = []
-            suit.forEach(suit => {
+            suits.forEach(suits => {
                 face.forEach(face => {
-                    deckOfCards.push(this.createDeck(suit, face))
+                    deckOfCards.push(this.createDeck(suits, face))
                 })
             })
             this.deckOfCards = deckOfCards
     }
-    createDeck(suit, face){
-        return `${face} of ${suit}`
+    createDeck(suits, face){
+        return `${face} of ${suits}`
     }
 }
 
 dealer = new Deck()
 console.log(dealer)
 
-
-
-//built in 3:51
-
-
+//built in 3:02
